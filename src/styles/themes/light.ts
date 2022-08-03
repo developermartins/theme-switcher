ad93d22e38c4e0dbc0cc0583ff3e17608634ecaf
martsDev <martssilvapro@gmail.com>
2022-08-03 15:14:01 -0300

    setup light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#453750',
+        secundary: '#73648a',
+
+        background: '#F5F5F5',
+        text: '#333',
+    },
+};
