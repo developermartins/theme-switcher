ee35987f510e8871bc2c14e4ba5298e9fe1665e7
martsDev <martssilvapro@gmail.com>
2022-08-03 16:20:07 -0300

    add dark theme file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components';
 import light from './styles/themes/light';
+import dark from  './styles/themes/dark';
+import { useState } from 'react';
 
 function App() {
+
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
   return (
     <div className="App">
         <ThemeProvider theme={light}>
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,13 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#453750',
+        secundary: '#73648a',
+        themeOn: '#9882AC',
+        themeOff: '#A393BF',
+
+        background: '#F5F5F5',
+        text: '#333',
+    },
+};
